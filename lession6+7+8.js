57c637e0317f5d337aab54e5058325f3e3790a1c
Alexandr Dragin <one.eastside@gmail.com>
2016-02-05 21:04:16 +0300

    7

--- lession6+7+8.js
+++ lession6+7+8.js
@@ -1,3 +1,67 @@
+6
+События
+Модель обработки событий DOM Level 2:
+обработчики событий, фазы события, объект Event.
+ Делегирование событий
+
+
+ arr.slice(0, 5) = []/выбока от 0 до 4
+arr.slice(0, -5) = []/ограничивает спраправа количество элементов
+
+DOM Level 2
+
+множественные удаляеммые
+фазы
+собственные события
+
+obj.addEventListener('click', function(evt)) {
+
+};
+
+obj.removeEventListener('click', function(evt)) { //удалить только если функция именная
+
+}
+
+троттл
+
+
+делегирование
+всплытие
+или на фазе захвата
+те захват испускание и всплытие(третий параметр - фалс на всплытие/ тру на захвате)
+
+
+
+кастомные события
+CustomEvent
+var newEvent = new CustomEvent ('nameEvent');
+window.dispatchEvent(newEvent);
+window.addEventListener('nameEvent', function(){
+
+});
+
+// для сафари
+var newEvent = document.createEvent('CustomEvent');
+newEvent.initCustomEvent('evettype', false);
+
+window.addEventListener('evettype', function () {
+
+});
+
+window.dispatchEvent(evt);
+
+//
+createEvent('Event');
+initEvent('click');
+
+
+
+7
+
+
+
+
+
 
 8
 графика и медиа
